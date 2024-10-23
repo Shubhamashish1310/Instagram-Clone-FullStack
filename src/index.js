e178e87a07c4121450a802080d6d9245fd937f69
Shubhamashish1310 <98949989+Shubhamashish1310@users.noreply.github.com>
2024-10-23 23:38:48 +0530

    added use and middleware things with req body

--- src/index.js
+++ src/index.js
@@ -6,7 +6,29 @@ const port = 3000;
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+function m1(req,res,next){
+    console.log("m1");
+    next();
+}
+function m2(req,res,next){
+    console.log("m2");
+    next();
+}
+function m3(req,res,next){
+    console.log("m3");
+    next();
+}
+
+app.use(m1,m2,m3);
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.text());
+
 app.get('/test', (req, res) => {
+    console.log(req.query);
+    console.log(req.body);
     res.json({
         name: 'test',
         age: 18,
